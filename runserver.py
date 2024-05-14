513196f9fc83dad51be76c92fe4c05be89d1907c
Ahmed Adel <69484554+Ahmed23Adel@users.noreply.github.com>
2024-05-14 15:12:07 +0300

    Restructuring the project for deployment

--- runserver.py
+++ runserver.py
@@ -1,4 +1,5 @@
 import uvicorn
-
+import os
 if __name__ =="__main__":
-    uvicorn.run(app="src:app", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))  # Use the PORT environment variable or default to 8000
+    uvicorn.run("src:app", host="0.0.0.0", port=port, reload=True)
